d6bea08a414371ca084af26c6a3bb1ca299c5fef
Paul Meyer <paul.meyer@microsoft.com>
2015-11-13 10:03:50 -0800

    fmt

--- management/virtualmachine/client.go
+++ management/virtualmachine/client.go
@@ -9,14 +9,14 @@ import (
 )
 
 const (
-	azureDeploymentListURL   = "services/hostedservices/%s/deployments"
-	azureDeploymentURL       = "services/hostedservices/%s/deployments/%s"
+	azureDeploymentListURL        = "services/hostedservices/%s/deployments"
+	azureDeploymentURL            = "services/hostedservices/%s/deployments/%s"
 	azureListDeploymentsInSlotURL = "services/hostedservices/%s/deploymentslots/Production"
-	deleteAzureDeploymentURL = "services/hostedservices/%s/deployments/%s?comp=media"
-	azureAddRoleURL          = "services/hostedservices/%s/deployments/%s/roles"
-	azureRoleURL             = "services/hostedservices/%s/deployments/%s/roles/%s"
-	azureOperationsURL       = "services/hostedservices/%s/deployments/%s/roleinstances/%s/Operations"
-	azureRoleSizeListURL     = "rolesizes"
+	deleteAzureDeploymentURL      = "services/hostedservices/%s/deployments/%s?comp=media"
+	azureAddRoleURL               = "services/hostedservices/%s/deployments/%s/roles"
+	azureRoleURL                  = "services/hostedservices/%s/deployments/%s/roles/%s"
+	azureOperationsURL            = "services/hostedservices/%s/deployments/%s/roleinstances/%s/Operations"
+	azureRoleSizeListURL          = "rolesizes"
 
 	errParamNotSpecified = "Parameter %s is not specified."
 )
@@ -81,7 +81,7 @@ func (vm VirtualMachineClient) GetDeploymentName(cloudServiceName string) (strin
 		} else {
 			return "", err
 		}
-        }
+	}
 	err = xml.Unmarshal(response, &deployment)
 	if err != nil {
 		return "", err
